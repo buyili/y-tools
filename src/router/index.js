506d99feee80efc2d4e46bf42419fe6a7ac4942e
Limaoxu <1132989278@qq.com>
2023-06-03 17:25:33 +0800

    feat: change router history mode

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,8 @@ const routes = [
 
 const router = VueRouter.createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: VueRouter.createWebHistory(),
+  history: VueRouter.createWebHashHistory(),
+  // history: VueRouter.createWebHistory(),
   routes, // `routes: routes` 的缩写
 })
 
